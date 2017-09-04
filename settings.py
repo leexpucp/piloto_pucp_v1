f280c96e2a7eb1361adc62ac14f9e8c7a60bb57e
Carlo1911 <carlo.alva@pucp.pe>
2017-09-04 12:46:17 -0500

    ad rooms

--- settings.py
+++ settings.py
@@ -90,6 +90,11 @@ DEMO_PAGE_INTRO_TEXT = """
 
 ROOMS = [
     {
+        'name': 'EconoLab',
+        'display_name': 'Laboratorio de Economía',
+        'participant_label_file': '_rooms/econolab.txt',
+    },
+    {
         'name': 'ECO315',
         'display_name': 'Grupo de Economía Conductual y Experimental',
         'participant_label_file': '_rooms/ECO315.txt',
